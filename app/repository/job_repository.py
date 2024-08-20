aba9971a8d5ba3f8cfac1d6d69f1a39298752df4
Abu Darda <abu.darda@brainstation-23.com>
2024-08-20 18:48:43 +0600

    all jobs endpoints

--- app/repository/job_repository.py
+++ app/repository/job_repository.py
@@ -3,9 +3,21 @@ from requests import Session
 from utils.logger import logger
 from app.models.job import Job
 from app.db import engine, SessionLocal, Base, get_db
-from app.schema.job import JobBase
+from app.schema.job import *
 
 class JobRepository:
+    @staticmethod
+    async def get_all_jobs():
+        db = SessionLocal()
+        try:
+            jobs = db.query(Job).all()
+            return jobs
+        except HTTPException as e:
+            logger.info(f'An HTTP error occurred: \n {str(e)}')
+            raise e
+        finally:
+            db.close()
+
     @staticmethod
     async def create_job(job: JobBase):
         db = SessionLocal()
@@ -14,6 +26,56 @@ class JobRepository:
             db.add(db_job)
             db.commit()
             db.refresh(db_job) 
+            return db_job
+        except HTTPException as e:
+            logger.info(f'An HTTP error occurred: \n {str(e)}')
+            raise e
+        finally:
+            db.close()
+
+    @staticmethod
+    async def get_job_by_id(job_id: int):
+        db = SessionLocal()
+        try:
+            job = db.query(Job).filter(Job.id == job_id).first()
+            return job
+        except HTTPException as e:
+            logger.info(f'An HTTP error occurred: \n {str(e)}')
+            raise e
+        finally:
+            db.close()
+
+    @staticmethod
+    async def update_job(job_id: int, job: JobUpdate):
+        db = SessionLocal()
+        try:
+            db_job = db.query(Job).filter(Job.id == job_id).first()
+            if not db_job:
+                raise HTTPException(status_code=404, detail="Job not found")
+            if(job.name):
+                db_job.name = job.name
+            if(job.description):
+                db_job.description = job.description
+            if(job.user_id):
+                db_job.user_id = job.user_id
+            db.commit()
+            db.refresh(db_job)
+            return db_job
+        except HTTPException as e:
+            logger.info(f'An HTTP error occurred: \n {str(e)}')
+            raise e
+        finally:
+            db.close()
+
+    @staticmethod
+    async def delete_job(job_id):
+        db = SessionLocal()
+        try:
+            db_job = db.query(Job).filter(Job.id == job_id).first()
+            if not db_job:
+                raise HTTPException(status_code=404, detail="Job not found")
+            db.delete(db_job)
+            db.commit()
             return db_job
         except HTTPException as e:
             logger.info(f'An HTTP error occurred: \n {str(e)}')
